1a20da55d2baaee3698df2f816c34bd282020b3e
jamesNicholasRyan <j.n.ryan@hotmail.co.uk>
2021-02-08 13:42:21 +0000

    changed BroswerRouter to - HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Switch, Route, Link } from 'react-router-dom'
 import Home from './components/Home'
 import Nav from './components/Nav'
 import Circuits from './components/Circuits'
@@ -8,14 +8,14 @@ import Circuit from './components/Circuit'
 import './styles/style.scss'
 
 const App = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Nav />
     <Switch>
       <Route exact path="/F1data/circuits/:id" component={Circuit}/>
       <Route exact path="/F1data/circuits" component={Circuits}/>
       <Route exact path="/F1data/" component={Home}/>
     </Switch>
-  </BrowserRouter>
+  </HashRouter>
 )
 
 // http://localhost:8000/circuits/circuit?id=anderstorp?year=2016
